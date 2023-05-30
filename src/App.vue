42cec7d215882947f7218fc3210f59182df773e4
kevinwu <634103089@qq.com>
2023-05-30 19:32:41 +0800

    axios二次封装：请求响应拦截器

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,20 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import request from '@/utils/request.ts'
+onMounted(() => {
+  request({
+    url: '/user/login1',
+    method: 'post',
+    data: {
+      username: 'admin',
+      password: '111111',
+    },
+  }).then((v) => console.log(v))
+})
+</script>
 
 <style lang="scss" scoped>
 .outer {

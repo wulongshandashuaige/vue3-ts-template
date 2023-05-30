e967e50fbe80b94cc8c15bed40d54d3e801a8dee
kevinwu <634103089@qq.com>
2023-05-30 15:01:26 +0800

    集成element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  Check,
+  Delete,
+  Edit,
+  Message,
+  Search,
+  Star,
+} from '@element-plus/icons-vue'</script>
 
 <template>
-  <div>App</div>
+  <div>App
+    <el-button :icon="Search" circle />
+    <el-button type="primary" :icon="Edit" circle />
+    <el-button type="success" :icon="Check" circle />
+    <el-button type="info" :icon="Message" circle />
+    <el-button type="warning" :icon="Star" circle />
+    <el-button type="danger" :icon="Delete" circle />
+  </div>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// @ts-ignore忽略类型检测
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn
+})
+app.mount('#app')
